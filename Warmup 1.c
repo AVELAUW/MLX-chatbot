fea365b42e3cbc76b3709b5a016e9f7dac51801a
Ruth Amaha <ramaha22@uw.edu>
2026-06-23 08:36:19 -0700

    Create Warmup 1.c

---
+++ Warmup 1.c
@@ -0,0 +1,26 @@
+
+let allergens = ["peanut", "gluten"]  //DO NOT ALTER
+
+// PSEUDOCODE:
+//   define a function called printAllergens
+//       for each allergen in the allergens list
+//           print the allergen
+//       end loop
+//   end function
+func printAllergens() {
+    // YOUR CODE HERE (replace with ~3 lines)
+}
+
+// PSEUDOCODE:
+//   define a function called checkIngredients that takes an array of Strings and returns Bool
+//       for each ingredient in the list
+//           if the allergens list contains this ingredient
+//               return true — match found
+//           end if
+//       end loop
+//       return false — no match
+//   end function
+func checkIngredients(_ ingredients: [String]) -> Bool {
+    // YOUR CODE HERE (replace with ~5 lines)
+    return false
+}
