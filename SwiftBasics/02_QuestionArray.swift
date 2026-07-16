f5089504c27ebeb6db2d9334fb1fc9c2202618c4
Ruth Amaha <ramaha22@uw.edu>
2026-07-16 00:06:33 -0700

    Revise section header and question instructions
    Updated section header and instructions for adding questions.

--- SwiftBasics/02_QuestionArray.swift
+++ SwiftBasics/02_QuestionArray.swift
@@ -1,14 +1,14 @@
-//  SECTION 4: Question Buttons
+//  SECTION 2: Question Buttons
 
 // ★ Add more questions to this array.
 //   Each string becomes a tappable button on screen.
 //   EXAMPLES: "Who uses data activism?", "Why does data matter?"
 
-//  COPY & PASTE code below, replace //Section 2:, add your own questions.
+//  COPY & PASTE code below in //Section 2:, replace ?'s your own questions.
 
     private let suggestedQuestions = [
-        " ",
-        " ",
+        " ? ",
+        " ? ",
         //ADD YOUR QUESTIONS BELOW (comma after each one)
 
     ]
